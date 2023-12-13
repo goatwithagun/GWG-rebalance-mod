be77f6449bf0bb2e661ec5deae30fba87d15ab2f
goatwithagun <153561950+goatwithagun@users.noreply.github.com>
2023-12-13 17:38:43 -0300

    Delete hooks directory

--- hooks/achivementmanager.lua
+++
@@ -1,7 +0,0 @@
-old_award = AchievmentManager.award
-function AchievmentManager:award(id)
-	if Global.game_settings and Global.game_settings.difficulty == 'sm_wish' then
-		return
-	end
-	old_award(self, id)
-end
