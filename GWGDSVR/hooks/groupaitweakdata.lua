caa8f2ac215937acda079d1fd4e9083a61710cb0
goatwithagun <153561950+goatwithagun@users.noreply.github.com>
2023-12-13 17:40:17 -0300

    i fuckent up

---
+++ GWGDSVR/hooks/groupaitweakdata.lua
@@ -0,0 +1,21 @@
+local old_init = GroupAITweakData.init
+function GroupAITweakData:init(tweak_data)
+	old_init(self, tweak_data)
+	local difficulty = Global.game_settings and Global.game_settings.difficulty or "normal"
+	if difficulty == "sm_wish" then		
+	
+			self.special_unit_spawn_limits = {
+				tank = 2,
+				taser = 4,
+				spooc = 2,
+				shield = 4,
+				medic = 2
+			}
+		--better way to do it than overriding the function and messing up all the spawn groups
+		--this way avoids updates breaking things, such as adding a new ai type like zombie during halloween
+		table.remove(self.unit_categories.marshal_marksman.unit_types.america, 1)
+		table.insert(self.unit_categories.marshal_marksman.unit_types.america, Idstring("units/pd2_dlc_drm/characters/ene_zeal_swat_heavy_sniper/ene_zeal_swat_heavy_sniper"))
+	end
+
+end
+
