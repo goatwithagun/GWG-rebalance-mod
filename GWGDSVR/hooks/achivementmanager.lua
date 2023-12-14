849f1c0ac3416df52314c5c45ab649a71caff805
goatwithagun <153561950+goatwithagun@users.noreply.github.com>
2023-12-13 22:05:48 -0300

    hopefully fixing the achivement blocking

--- GWGDSVR/hooks/achivementmanager.lua
+++ GWGDSVR/hooks/achivementmanager.lua
@@ -1,7 +1,2 @@
-old_award = AchievmentManager.award
-function AchievmentManager:award(id)
-	if Global.game_settings and Global.game_settings.difficulty == 'sm_wish' then
-		return
-	end
-	old_award(self, id)
-end
+function AchievmentManager:award_progress() end
+function AchievmentManager:award() end
