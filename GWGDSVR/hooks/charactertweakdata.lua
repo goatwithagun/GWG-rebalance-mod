834be77f80859ea4084f9c7a85a3e8eacecfa71c
goatwithagun <153561950+goatwithagun@users.noreply.github.com>
2023-12-14 12:53:08 -0300

    Update charactertweakdata.lua

--- GWGDSVR/hooks/charactertweakdata.lua
+++ GWGDSVR/hooks/charactertweakdata.lua
@@ -4,7 +4,7 @@ Hooks:PostHook(CharacterTweakData,"init","mark_init",function(self)
 	self.heavy_swat_sniper.tags = {"law","sniper","special"}
 	self.heavy_swat_sniper.priority_shout = "f34"
 	self.heavy_swat_sniper.surrender = false
-	self.heavy_swat_sniper.HEALTH_INIT = 40
+	self.heavy_swat_sniper.HEALTH_INIT = 30
 	self.heavy_swat_sniper.suppression = false
 	
 	--medic dozer nerf
